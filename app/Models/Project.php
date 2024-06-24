a6c7648877cb68460d3f18431e6192fbada082ea
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-06-24 16:25:36 +0200

    feat: created migration and model for Type and linked the types table to projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable=['title','owner','description'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class); // Una Car appartiene a un Brand
+    }
 }
